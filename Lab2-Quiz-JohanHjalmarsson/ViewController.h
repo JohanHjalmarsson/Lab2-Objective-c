5cdcf94450f205e3c901f67587aecda1e573f387
JohanHjalmarsson <johan.g.hjalmarsson@gmail.com>
2018-01-23 14:11:47 +0100

    Initial Commit

---
+++ Lab2-Quiz-JohanHjalmarsson/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Lab2-Quiz-JohanHjalmarsson
+//
+//  Created by Johan Hjalmarsson on 2018-01-23.
+//  Copyright © 2018 Johan Hjalmarsson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Lab2-Quiz-JohanHjalmarsson/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Lab2-Quiz-JohanHjalmarsson
+//
+//  Created by Johan Hjalmarsson on 2018-01-23.
+//  Copyright © 2018 Johan Hjalmarsson. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
